4c808edc79bb924653a100b8fd7364416928856d
Yoicer <36748297+Yoicer@users.noreply.github.com>
2018-03-01 23:33:05 -0500

    Update LexerNoClass.py

--- Lexico/LexerNoClass.py
+++ Lexico/LexerNoClass.py
@@ -6,15 +6,13 @@ resultado_lexema = []
 reservada = (
     # Palabras Reservadas
     'INCLUYENTE',
-    'UTILIZAR',
     'MOSTRAR',
     'LEER',
     'OPTENER',
-    'CADENA',
     'DEVUELVE',
     'VACIO',
     'ENTERO',
-    'SALTO',
+    'BOOL',
 )
 tokens = reservada + (
     'IDENTIFICADOR',
@@ -34,7 +32,7 @@ tokens = reservada + (
     'PARA',
     'Y',
     'O',
-    'NO',
+    'NEGACION',
     'ELMENOR',
     'MENOROIGUAL',
     'ELMAYOR',
@@ -42,14 +40,12 @@ tokens = reservada + (
     'IGUAL',
     'DESIGUAL',
     
-    'HASHTAG',
     'PARENIZQ',
     'PARENDER',
     'LLAVEIZQ',
     'LLAVEDER',
-    'PUNTOYCOMA',
-    'COMA',
-    'COMADOB',
+    'FINALDESENTENCIA',
+  
 )
 
 # Reglas de Expresiones Regualres para token de Contexto simple
@@ -60,56 +56,52 @@ t_MULTIPLICADOR = r'\*'
 t_DIVISOR = r'/'
 t_MODULO = r'\%'
 t_POTENCIADOR = r'(\*{2} | \^)'
-
 t_ASIGNADOR = r'='
 
 
-def t_INCLUYENTE(t):
-    r'incluyente'
-    return t
 
-def t_UTILIZAR(t):
-    r'utilizar'
+def t_INCLUYENTE(t):
+    r'incl'
     return t
 
 def t_MOSTRAR(t):
-    r'cout'
+    r'msg'
     return t
 
 def t_LEER(t):
-    r'cin'
+    r'rec'
     return t
 
 def t_OPTENER(t):
-    r'get'
+    r'opt'
     return t
 
-def t_SALTO(t):
-    r'endl'
+def t_BOOL(t):
+    r'bool'
     return t
 
 def t_YSINO(t):
-    r'else'
+    r'sino'
     return t
 
 def t_SI(t):
-    r'if'
+    r'si'
     return t
 
 def t_DEVUELVE(t):
-   r'return'
+   r'retorna'
    return t
 
 def t_VACIO(t):
-   r'void'
+   r'vac'
    return t
 
 def t_MIENTRAS(t):
-    r'while'
+    r'mtras'
     return t
 
 def t_PARA(t):
-    r'for'
+    r'para'
     return t
 
 def t_ENTERO(t):
@@ -118,7 +110,7 @@ def t_ENTERO(t):
     return t
 
 def t_IDENTIFICADOR(t):
-    r'\w+(_\d\w)*'
+    r'[a-zA-Z_][a-zA-Z0-9]*'
     return t
 
 def t_newline(t):
@@ -126,9 +118,15 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 def t_comments(t):
-    r'\//'
-    t.lexer.lineno += t.value.count('\n+')
-    print("Comentario de multiple linea")
+    r'\$.*'
+    pass
+
+def t_error(t):
+    global resultado_lexema
+    estado = "Token no valido en la Linea: {:4}     Valor: {:16}    Posicion: {:4}".format(str(t.lineno), str(t.value),str(t.lexpos))
+    resultado_lexema.append(estado)
+    t.lexer.skip(1)
+
 
 
 
